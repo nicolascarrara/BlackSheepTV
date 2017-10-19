4fbab7107780a0566290a1178da896549f03b13b
Nicolas CARRARA <n.carrara28@icloud.com>
2017-10-19 11:59:48 +0200

    test api

--- blacksheep/urls.py
+++ blacksheep/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from . import views
-from .views import accueil
+from .views import accueil, test
 
 
 app_name = 'blacksheep'
 urlpatterns = [
     url(r'^$', accueil, name='accueil'),
-
+    url(r'^test/', test, name='test'),
 ]
--- blacksheep/views.py
+++ blacksheep/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse
+from BlackSheepTV import settings
+import requests
+
 # Create your views here.
 
 
 def accueil(request):
     return render(request, 'blacksheep/accueil.html')
+
+
+def test(request):
+    parameters = {'apikey': "C9D85668BADC5AD4",
+                  'userkey': "04565F600F78B2C0", 'username': "nicolascarrara"}
+    response = requests.post(
+        "https://api.thetvdb.com/login", params=parameters)
+    data = response.json()
+    """
+    decoders = [
+        codecs.JSONCodec()
+    ]
+    client = coreapi.Client(decoders=decoders)
+    schema = client.get('https://api.thetvdb.com/login')
+
+    action = ['api-token-auth', 'obtain-token']
+    params = {'apikey': "C9D85668BADC5AD4",
+              'userkey': "04565F600F78B2C0", 'username': "nicolascarrara"}
+    result = client.action(schema, action, params)
+
+    auth = coreapi.auth.TokenAuthentication(
+        scheme='JWT',
+        token=result['token']
+    )
+    client = coreapi.Client(auth=auth)"""
+    return render(request, data, 'blacksheep/test.html')
