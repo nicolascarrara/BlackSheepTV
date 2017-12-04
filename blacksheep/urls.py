f1c61da7d28a70d7700f0d8d3a71eb53ef0941ab
Amael ZICKLER <amaelzickler@gmail.com>
2017-12-04 10:32:33 +0100

    css

--- blacksheep/urls.py
+++ blacksheep/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import rechercheFilm, rechercheSerie,accueil, FilmListView, SerieListView, FilmDetailView, SerieDetailView, SaisonDetailView, EpisodeDetailView,loginAPI,search
+from .views import rechercheFilm, rechercheSerie,accueil, FilmListView, SerieListView, FilmDetailView, SerieDetailView, SaisonDetailView, EpisodeDetailView,loginAPI
 
 
 
@@ -7,7 +7,6 @@ app_name = 'blacksheep'
 urlpatterns = [
     url(r'^$', accueil, name='accueil'),
     url(r'^loginAPI/', loginAPI, name='loginAPI'),
-    url(r'^search/', search, name='search'),
     url(r'^film$', FilmListView.as_view(), name='film-list'),
     url(r'^film/search', rechercheFilm, name='film-search'),
     url(r'^detailFilm/(?P<pk>\d+)$', FilmDetailView.as_view(), name='film-detail'),
