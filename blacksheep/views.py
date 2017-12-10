ce15ea2cef9ceb36a91ab045afe96638a3b64567
Nicolas CARRARA <n.carrara28@icloud.com>
2017-12-10 12:25:55 +0100

    indentation et design detailfilm

--- blacksheep/views.py
+++ blacksheep/views.py
@@ -119,10 +119,8 @@ def SerieList(request):
     try:
         series = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
         series = paginator.page(1)
     except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
         series = paginator.page(paginator.num_pages)
 
     return render(request, 'blacksheep/serie_list.html', {'object_list': series})
